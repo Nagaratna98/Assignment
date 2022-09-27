5249c295009dfd08353ab0798746e14fdf5d650c
Nagaratna98 <106804983+Nagaratna98@users.noreply.github.com>
2022-09-27 15:26:30 +0530

    Added 1st aasignment

---
+++ Assignment_java_1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Pgm1 {
+  public static void main(String[] args) {
+
+    char operator;
+    Double a, b, result;
+
+
+    Scanner input = new Scanner(System.in);
+
+
+
+
+    System.out.println("Enter first number");
+    a = input.nextDouble();
+
+    System.out.println("Enter second number");
+    b = input.nextDouble();
+
+    System.out.println("type of operation: +, -, *, or /");
+    operator = input.next().charAt(0);
+
+    switch (operator) {
+
+
+      case '+':
+        result = a + b;
+        System.out.println(a + " + " + b + " = " + result);
+        break;
+
+
+      case '-':
+        result = a - b;
+        System.out.println(a+ " - " + b+ " = " + result);
+        break;
+
+
+      case '*':
+        result = a * b;
+        System.out.println(a + " * " + b + " = " + result);
+        break;
+
+
+      case '/':
+        result = a / b;
+        System.out.println(a + " / " + b + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
